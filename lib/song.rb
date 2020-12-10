6fe38c0cacb9ec297a8ca8c6c0d81d8d4b6f68c1
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-10 19:14:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,20 @@
 class Song
-  
+  attr_accessor :name
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+
+  end
+
+  def self.all
+    @@all
+  end
+
+end
 end
