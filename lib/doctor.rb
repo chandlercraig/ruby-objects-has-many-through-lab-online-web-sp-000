a58e7a2565be8e315f0a375e70af09023227b5c2
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-10 19:36:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,4 +16,16 @@ class Doctor
     Appointment.new(date, patient, self)
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    self.appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+
 end
